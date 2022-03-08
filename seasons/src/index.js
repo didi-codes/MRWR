b1073d57a65b7c8f7ede5dd1154f4be0cea00906
Damaris Garcia <dgarciaprogrammer@gmail.com>
2022-03-08 16:55:07 -0500

    Addition of constructor and super and setting our state to a lateral degree of null

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
+  constructor(props) {
+    // Belongs to Javascript language, in a javascript class the constuctor function is the first function that is going to be called anytime an instance of this class is created
+    super(props); 
+    this.state = {lat: null };
+  }
   // When we make a class we are creating a new class inside of javascript that has one method assigned to it. React class component expects many other methods attached to it. We borrow these methods from React.Component. We extend to React.Component because it allows us to pull in built-in functionality into our class (subclassing or borrowing functionality into our app class)
   render() {
+    // React says we have to define render!!
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
       (err) => console.log(err)
@@ -26,5 +32,12 @@ ReactDOM.render(<App />, document.querySelector('#root'));
 3. Must define a 'render' method that returns some amount JSX
 */
 
+
 /* Rules of State
+1. Only usable with class components
+2. You will confuse props with state
+3. 'State' is a JS object that contains data relevant to a component
+4. Updating 'state' on a component causes the component to (almost) instantly rerender
+5. State must be initialized when a component os created
+6.State can only be updated using the function 'setState'
 */
