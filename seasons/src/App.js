510283737318c4aa59b8dc05c225b9d5345585cc
Damaris Garcia <dgarciaprogrammer@gmail.com>
2023-07-20 10:46:53 -0400

    Personal Digital Assistants Project Complete

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import 'bulma.css'
 import ProfileCard from './PDAS/ProfileCard'
 import Alexa from './PDAS/images/alexa.png'
 import Cortana from './PDAS/images/cortana.png'
@@ -8,12 +7,45 @@ import Siri from './PDAS/images/siri.png'
 function App() {
   return (
     <div>
-        <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard title='Alexa' handle='@alexa99' image={Alexa}/>
-            <ProfileCard title='Cortana' handle='@cortana32'image={Cortana} />
-            <ProfileCard title='Siri' handle='@siri01' image={Siri}/>
+      <div>
+        <section className="hero is-primary">
+          <div className="hero-body">
+            <p className="title">Personal Digital Assistants</p>
+          </div>
+        </section>
+        <div className="container">
+          <div className="section">
+            <div className="columns">
+              <div className="column is-3">
+                <ProfileCard 
+                title='Alexa' 
+                handle='@alexa99' 
+                description='Amazon'
+                image={Alexa} />
+              </div>
+
+              <div className="column is-3">
+                <ProfileCard 
+                title='Cortana'
+                 handle='@cortana32'
+                 description=' Microsoft' 
+                 image={Cortana} />
+              </div>
+
+              <div className="column is-3">
+                <ProfileCard 
+                title='Siri' 
+                handle='@siri01'
+                description='Apple' 
+                image={Siri} />
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+      <div>
+        
+      </div>
     </div>
   )
 }
--- seasons/src/PDAS/ProfileCard.js
+++ seasons/src/PDAS/ProfileCard.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import 'bulma/css/bulma.css'
 
-function ProfileCard({title, handle, image}) {
+function ProfileCard({title, handle, description, image}) {
   return (
-    <div>
-        <img src={image} alt=''/>
-        <div>{title}</div>
-        <div>{handle}</div>
-    </div>
+   
+      <div className='card'>
+        <div className='card-image'>
+          <figure className='image is-1by1'>
+            <img src={image} alt='pda logo' />
+          </figure>
+        </div>
+        <div className='card-content'>
+          <div className='media-content'>
+            <p className='title is-4'>{title}</p>
+            <p className='subtitle is-6'>{handle}</p>
+          </div>
+          <br/>
+          <div className="content">
+            {description}
+          </div>
+        </div>
+      </div>
   )
 }
 
